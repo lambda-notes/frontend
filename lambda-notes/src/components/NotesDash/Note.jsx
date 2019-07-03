aae51604ae07923102cfc5e98874b29efefeb14b
Mike Landers <landers.mike@gmail.com>
2019-07-03 07:12:41 -0700

    need to fix the ref

--- lambda-notes/src/components/NotesDash/Note.jsx
+++ lambda-notes/src/components/NotesDash/Note.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import { Editor } from 'slate-react';
 import Code from '@convertkit/slate-code';
 import PasteLinkify from 'slate-paste-linkify';
 import InsertImages from 'slate-drop-or-paste-images';
 import DropOrPasteImages from 'slate-drop-or-paste-images';
+import { isKeyHotkey } from 'is-hotkey';
+import { Button, Icon, Toolbar } from './components';
 
 import { useStateValue } from 'react-conflux';
 import { notesContext } from '../../store/contexts';
@@ -86,13 +88,31 @@ const plugins = [
     }
   })
 ];
+/**
+ * Define the default node type.
+ *
+ * @type {String}
+ */
+
+const DEFAULT_NODE = 'paragraph';
+
+/**
+ * Define hotkey matchers.
+ *
+ * @type {Function}
+ */
+
+const isBoldHotkey = isKeyHotkey('mod+b');
+const isItalicHotkey = isKeyHotkey('mod+i');
+const isUnderlinedHotkey = isKeyHotkey('mod+u');
+const isCodeHotkey = isKeyHotkey('mod+`');
 
 const Note = props => {
   const [state, dispatch] = useStateValue(notesContext);
+  const { note } = state.currentNote;
+  console.log(note);
 
   const onChange = ({ value }) => {
-    // console.log(state.currentNote.note.document.text);
-    // console.log(value.document.text);
     // Check to see if the document has changed before saving.
     if (value.document !== state.currentNote.note.document) {
       const content = JSON.stringify(value);
@@ -141,8 +161,38 @@ const Note = props => {
     dispatch({ type: SET_NOTE_TITLE, payload: e.target.value });
   };
 
-  // console.log(state.currentNote);
-  // console.log(Value.fromJSON(initialValue));
+  /**
+   * Check if the current selection has a mark with `type` in it.
+   *
+   * @param {String} type
+   * @return {Boolean}
+   */
+
+  const hasMark = type => {
+    // const { value } = this.state;
+    return note.activeMarks.some(mark => mark.type === type);
+  };
+
+  /**
+   * Check if the any of the currently selected blocks are of `type`.
+   *
+   * @param {String} type
+   * @return {Boolean}
+   */
+
+  const hasBlock = type => {
+    // const { value } = this.state;
+    return note.blocks.some(node => node.type === type);
+  };
+
+  /**
+   * Store a reference to the `editor`.
+   *
+   * @param {Editor} editor
+   */
+
+  let editor = useRef(null);
+
   return (
     <Styles>
       <input
@@ -151,18 +201,237 @@ const Note = props => {
         value={state.currentNote.noteTitle}
         onChange={handleChanges}
       />
+      <Toolbar>
+        {renderMarkButton('bold', 'format_bold')}
+        {renderMarkButton('italic', 'format_italic')}
+        {renderMarkButton('underlined', 'format_underlined')}
+        {renderMarkButton('code', 'code')}
+        {renderBlockButton('heading-one', 'looks_one')}
+        {renderBlockButton('heading-two', 'looks_two')}
+        {renderBlockButton('block-quote', 'format_quote')}
+        {renderBlockButton('numbered-list', 'format_list_numbered')}
+        {renderBlockButton('bulleted-list', 'format_list_bulleted')}
+      </Toolbar>
       <Editor
         className="editor"
+        spellCheck
+        autoFocus
         value={state.currentNote.note}
         onChange={onChange}
-        onKeyDown={handleKeyDown}
+        // onKeyDown={handleKeyDown}
+        onKeyDown={onKeyDown}
         commands={commands}
         queries={queries}
         renderNode={renderNode}
         plugins={plugins}
+        renderBlock={renderBlock}
+        renderMark={renderMark}
+        ref={editor}
       />
     </Styles>
   );
+
+  /**
+   * Render a mark-toggling toolbar button.
+   *
+   * @param {String} type
+   * @param {String} icon
+   * @return {Element}
+   */
+
+  function renderMarkButton(type, icon) {
+    const isActive = hasMark(type);
+
+    return (
+      <Button active={isActive} onMouseDown={event => onClickMark(event, type)}>
+        <Icon>{icon}</Icon>
+      </Button>
+    );
+  }
+
+  /**
+   * Render a block-toggling toolbar button.
+   *
+   * @param {String} type
+   * @param {String} icon
+   * @return {Element}
+   */
+
+  function renderBlockButton(type, icon) {
+    let isActive = hasBlock(type);
+
+    if (['numbered-list', 'bulleted-list'].includes(type)) {
+      const {
+        note: { document, blocks }
+      } = state.currentNote;
+
+      if (blocks.size > 0) {
+        const parent = document.getParent(blocks.first().key);
+        isActive = hasBlock('list-item') && parent && parent.type === type;
+      }
+    }
+
+    return (
+      <Button
+        active={isActive}
+        onMouseDown={event => onClickBlock(event, type)}
+      >
+        <Icon>{icon}</Icon>
+      </Button>
+    );
+  }
+
+  /**
+   * Render a Slate block.
+   *
+   * @param {Object} props
+   * @return {Element}
+   */
+
+  function renderBlock(props, editor, next) {
+    const { attributes, children, node } = props;
+
+    switch (node.type) {
+      case 'block-quote':
+        return <blockquote {...attributes}>{children}</blockquote>;
+      case 'bulleted-list':
+        return <ul {...attributes}>{children}</ul>;
+      case 'heading-one':
+        return <h1 {...attributes}>{children}</h1>;
+      case 'heading-two':
+        return <h2 {...attributes}>{children}</h2>;
+      case 'list-item':
+        return <li {...attributes}>{children}</li>;
+      case 'numbered-list':
+        return <ol {...attributes}>{children}</ol>;
+      default:
+        return next();
+    }
+  }
+
+  /**
+   * Render a Slate mark.
+   *
+   * @param {Object} props
+   * @return {Element}
+   */
+
+  function renderMark(props, editor, next) {
+    const { children, mark, attributes } = props;
+
+    switch (mark.type) {
+      case 'bold':
+        return <strong {...attributes}>{children}</strong>;
+      case 'code':
+        return <code {...attributes}>{children}</code>;
+      case 'italic':
+        return <em {...attributes}>{children}</em>;
+      case 'underlined':
+        return <u {...attributes}>{children}</u>;
+      default:
+        return next();
+    }
+  }
+
+  /**
+   * On change, save the new `value`.
+   *
+   * @param {Editor} editor
+   */
+
+  // onChange = ({ value }) => {
+  //   this.setState({ value })
+  // }
+
+  /**
+   * On key down, if it's a formatting command toggle a mark.
+   *
+   * @param {Event} event
+   * @param {Editor} editor
+   * @return {Change}
+   */
+
+  function onKeyDown(event, editor, next) {
+    let mark;
+
+    if (isBoldHotkey(event)) {
+      mark = 'bold';
+    } else if (isItalicHotkey(event)) {
+      mark = 'italic';
+    } else if (isUnderlinedHotkey(event)) {
+      mark = 'underlined';
+    } else if (isCodeHotkey(event)) {
+      mark = 'code';
+    } else {
+      return next();
+    }
+
+    event.preventDefault();
+    editor.toggleMark(mark);
+  }
+
+  /**
+   * When a mark button is clicked, toggle the current mark.
+   *
+   * @param {Event} event
+   * @param {String} type
+   */
+
+  function onClickMark(event, type) {
+    event.preventDefault();
+    editor.toggleMark(type);
+  }
+
+  /**
+   * When a block button is clicked, toggle the block type.
+   *
+   * @param {Event} event
+   * @param {String} type
+   */
+
+  function onClickBlock(event, type) {
+    event.preventDefault();
+
+    const { editor } = this;
+    const { value } = editor;
+    const { document } = value;
+
+    // Handle everything but list buttons.
+    if (type !== 'bulleted-list' && type !== 'numbered-list') {
+      const isActive = hasBlock(type);
+      const isList = hasBlock('list-item');
+
+      if (isList) {
+        editor
+          .setBlocks(isActive ? DEFAULT_NODE : type)
+          .unwrapBlock('bulleted-list')
+          .unwrapBlock('numbered-list');
+      } else {
+        editor.setBlocks(isActive ? DEFAULT_NODE : type);
+      }
+    } else {
+      // Handle the extra wrapping required for list buttons.
+      const isList = hasBlock('list-item');
+      const isType = value.blocks.some(block => {
+        return !!document.getClosest(block.key, parent => parent.type === type);
+      });
+
+      if (isList && isType) {
+        editor
+          .setBlocks(DEFAULT_NODE)
+          .unwrapBlock('bulleted-list')
+          .unwrapBlock('numbered-list');
+      } else if (isList) {
+        editor
+          .unwrapBlock(
+            type === 'bulleted-list' ? 'numbered-list' : 'bulleted-list'
+          )
+          .wrapBlock(type);
+      } else {
+        editor.setBlocks('list-item').wrapBlock(type);
+      }
+    }
+  }
 };
 
 export default Note;
